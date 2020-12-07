fe55df4d2cdbcd100b7726f2385e2f690a98eb76
u-235 <nicegorov@yandex.ru>
2020-12-07 15:54:06 +0300

    Исправление ошибочного поведения.

--- src/methods/step_down.c
+++ src/methods/step_down.c
@@ -25,7 +25,6 @@ method_step_down(const street_t *street)
         return false;
     }
 
-    int first_highest = street->highest_first;
     /* Количество однозначно видимых построенных зданий текущего ряда.*/
     int total_visible = street->visible;
     /* Общее количество строящихся зданий, которые могут повлиять на видимость.*/
@@ -37,12 +36,12 @@ method_step_down(const street_t *street)
     int hill_cnt = street->hill_count;
 
 
-    if (!total_vacant || clue - total_visible != hill_cnt) {
+    if (!total_vacant || clue - total_visible != 1) {
         return false;
     }
 
     for (int hl_i = 0; hl_i < hill_cnt; hl_i++) {
-        if (hills[hl_i].vacant != 1) {
+        if (hills[hl_i].vacant != total_vacant) {
             continue;
         }
 
@@ -56,14 +55,18 @@ method_step_down(const street_t *street)
         int enable_bit = 1;
         int enable_mask = hills[hl_i].action_mask;
 
-        for (int tw_i = hills[hl_i].first + 1; tw_i <= hills[hl_i].last
-                && tw_i < first_highest; tw_i++) {
+        for (int tw_i = hills[hl_i].first + 1; tw_i <= hills[hl_i].last; tw_i++) {
             enable_bit <<= 1;
 
             if ((enable_mask & enable_bit) != 0) {
                 tower_t *tower = street_get_tower(street, tw_i);
 
-                if (tower->height == 0 && tower_and_options(tower, mask_and)) {
+                /* TODO без проверки tower_has_floors в некоторых случаях tower_and_options
+                 * вызывает ошибку "tower_set_options: Assertion `options != 0' failed."
+                 * Скорее всего это происходит при переборе в неправильном направлении. */
+                if (!tower_is_complete(tower) &&
+                        tower_has_floors(tower, mask_and) &&
+                        tower_and_options(tower, mask_and)) {
                     changed = true;
                 }
             }
